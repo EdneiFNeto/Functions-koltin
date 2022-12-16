e19c3d69c5b362e491c02fd203f0e40f1852aeba
EdneiFNeto <edneifneto@gmail.com>
2022-12-16 10:47:29 -0300

    Function align

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,15 @@ const val TAMANHO_PAPEL = 32.0
 
 fun main(args: Array<String>) {
 
-    val alignCenter = "Texto".alignCenter()
+    val number = "123456"
+
+    val first = number.substring(1)
+    val last = number.substring(0, number.length - 1)
+
+    println("first = $first")
+    println("last = $last")
+
+    val alignCenter = "Jose Augusto de Souza".alignCenter()
     println(alignCenter)
 
 }
@@ -30,5 +38,32 @@ fun String.alignCenter(): String {
     println("spaceLeft = ${spaceLeft.length}")
     println("spaceRight = ${spaceRight.length}")
 
-    return "$spaceLeft$this$spaceRight"
+    val final = "$spaceLeft$this$spaceRight"
+
+    println("final[before]: ${final.length}")
+
+    if (final.length > TAMANHO_PAPEL) {
+        println("Final > TAMANHO_PAPEL")
+        println("Final ${final.length}")
+        println("TAMANHO_PAPEL $TAMANHO_PAPEL")
+
+        println("Diff ${( final.length -TAMANHO_PAPEL).roundToInt()}")
+
+       return  when (val diff = (final.length - TAMANHO_PAPEL).roundToInt()) {
+            1 -> final.substring(diff)
+
+            else -> {
+                when {
+                    diff > 1 -> {
+                        val roundToInt = (diff / 2)
+                        final.substring(roundToInt)
+                        final.substring(0, final.length - roundToInt)
+                    }
+                    else -> final
+                }
+            }
+        }
+    }
+
+    return final
 }
